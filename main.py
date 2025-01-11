db424941dd8736844d8a528ffd1b9b238e743714
jrdncrm <jrdncrm@gmail.com>
2025-01-11 14:52:03 -0500

    Step 2: Triangle in Space

--- main.py
+++ main.py
@@ -3,19 +3,31 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
-	pygame.init
+	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting asteroids!")
 	print(f'Screen width: {SCREEN_WIDTH}')
 	print(f'Screen height: {SCREEN_HEIGHT}')
+	clock = pygame.time.Clock()
+	dt = 0
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
-		pygame.Surface.fill(screen,(0,0,0))
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+
+		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			return
+
+
+		clock.tick(60)
+		dt = clock.get_time() / 1000
 if __name__ == "__main__":
     main()
 
